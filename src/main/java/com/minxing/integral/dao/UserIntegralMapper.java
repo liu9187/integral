f8ee496b2967b248f46fd11bc5414a217e2cd75f
liuchanglong <842644810@qq.com>
2018-04-24 10:01:35 +0800

    新加入 积分新增 修改 积分显示页面 默认 排序 ASC升序

---
+++ src/main/java/com/minxing/integral/dao/UserIntegralMapper.java
@@ -0,0 +1,74 @@
+package com.minxing.integral.dao;
+
+import com.minxing.integral.common.bean.UserIntegral;
+import com.minxing.integral.common.pojo.vo.IntegralManagementVO;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 用户积分mapper
+ * @author liucl
+ * @date 2018-4-17
+ */
+@Component
+@Mapper
+public interface UserIntegralMapper {
+    /**
+     * 积分兑换
+     * @param userIntegral
+     * @return
+     */
+    @Update("UPDATE user_integral SET integral=integral-#{integral} WHERE user_id=#{userId}")
+    Integer  removeUserIntegralByUserId(UserIntegral userIntegral);
+
+    /**
+     * 积分管理 升序
+     * @return IntegralManagementVO
+     */
+    @Select("SELECT u.`name`,ui.integral,dept.short_name FROM users u " +
+            "LEFT JOIN  user_integral ui ON ui.user_id=u.id " +
+            "LEFT JOIN  departments dept  ON dept.id=u.dept_id " +
+            "ORDER BY ui.integral ")
+   List<IntegralManagementVO>  queryListByASC();
+    /**
+     * 积分管理 降序
+     * @return IntegralManagementVO
+     */
+    @Select("SELECT u.`name`,ui.integral,dept.short_name FROM users u " +
+            "LEFT JOIN  user_integral ui ON ui.user_id=u.id " +
+            "LEFT JOIN  departments dept  ON dept.id=u.dept_id " +
+            "ORDER BY ui.integral DESC")
+//    @Results(id = "learnMap", value = {
+//            @Result(column = "integral", property = "integral", javaType = Long.class),
+//            @Result(property = "short_name", column = "shortName", javaType = String.class),
+//            @Result(property = "name", column = "name", javaType = String.class)
+//    })
+    List<IntegralManagementVO> queryListByDESC();
+
+    /**
+     * 积分设置
+     * @param integralExchange
+     * @return
+     */
+    @Update("update integral_exchange SET integral_exchange=#{integral_exchange},create_date=NOW() WHERE id=1")
+    Integer updateIntegral(Integer integralExchange);
+
+    /**
+     * 通过userid 查询 积分
+     * @param userId
+     * @return
+     */
+    @Select("SELECT integral FROM user_integral WHERE user_id=#{userId}")
+    Long queryIntegralByUserId(Integer userId);
+
+    /**
+     * 通过usrid增加积分
+     * @param userIntegral
+     * @return
+     */
+    @Update("UPDATE user_integral SET integral=integral+#{integral} WHERE user_id=#{userId}")
+    Integer addIntegralByUserId(UserIntegral userIntegral);
+}
+
